6292fcce1d07d00cf0ddb22220ab6408036d9f8b
王晨阳 <1900353090@qq.com>
2021-11-07 18:00:26 +0800

    更新工具类

---
+++ src/main/java/cn/wcy/config/RedisConfig.java
@@ -0,0 +1,101 @@
+package cn.wcy.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+
+/**
+ * <p>Title : RedisConfig.java</p>
+ * <p>Description : redis缓存配置</p>
+ * <p>DevelopTools : IntelliJ IDEA 2018.2.3 x64</p>
+ * <p>DevelopSystem : Windows 10</p>
+ * <p>Company : org.wcy</p>
+ * @author : WangChenYang
+ * @date : 2018/12/28 10:16
+ * @version : 0.0.1
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    //@Bean
+    //public RedisService redisService() {
+    //    return new RedisService();
+    //}
+
+    /*
+     * 定义缓存数据 key 生成策略的bean 包名+类名+方法名+所有参数
+     */
+    @Bean
+    public KeyGenerator wiselyKeyGenerator(){
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+
+    }
+
+    /*
+     * 要启用spring缓存支持,需创建一个 CacheManager的 bean，CacheManager 接口有很多实现，这里Redis 的集成，用
+     * RedisCacheManager这个实现类 Redis 不是应用的共享内存，它只是一个内存服务器，就像 MySql 似的，
+     * 我们需要将应用连接到它，因此我们通过redis的连接工厂，创建一个redis管理对象
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheManager cacheManager = RedisCacheManager.create(redisConnectionFactory);
+        // cacheManager.setDefaultExpiration(60);//设置缓存保留时间（seconds）
+        return cacheManager;
+    }
+
+    // 1.项目启动时此方法先被注册成bean被spring管理,如果没有这个bean，则redis可视化工具中的中文内容（key或者value）都会以二进制存储，不易检查。
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        //配置连接工厂
+        template.setConnectionFactory(factory);
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // 值采用json序列化
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+
+        //设置redis支持数据库的事务
+//        template.setEnableTransactionSupport(true);
+
+        // 设置hash key 和value序列化模式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
